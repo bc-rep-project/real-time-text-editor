2b1e1f3e7c6f99b51ed2033d086e8bdb91f50865
Robocoders <team@blackbox.ai>
2024-10-20 12:33:50 +0000

    Update

--- web-app/server.js
+++ web-app/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const next = require('next');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { server: websocketServer, app: websocketApp } = require('./web-app/websocket');
-const apiRoutes = require('./web-app/api');
+const { server: websocketServer, app: websocketApp } = require('./websocket');
+const apiRoutes = require('./api');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, dir: './web-app' });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
