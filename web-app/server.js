77c2174af9b3330c329c3b9a830840ce9d8534ae
Robocoders <team@blackbox.ai>
2024-10-20 12:30:25 +0000

    Update

--- web-app/server.js
+++ web-app/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const next = require('next');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { server: websocketServer, app: websocketApp } = require('./websocket');
 const apiRoutes = require('./api');
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -27,8 +26,7 @@ app.prepare().then(() => {
 
   const PORT = process.env.PORT || 3003;
 
-  websocketServer.on('request', server);
-  websocketServer.listen(PORT, (err) => {
+  server.listen(PORT, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${PORT}`);
   });
